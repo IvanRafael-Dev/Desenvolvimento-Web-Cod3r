a9a08561d895a273d2d5ed0e8362ff690b0a6203
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-16 11:56:45 -0300

    Estudo sql - cria tabela estados'

---
+++ 488_CriarTabelaEstados/banco-relacional.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE estados (
+--     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     nome varchar(45) NOT NULL,
+--     sigla varchar(2) NOT NULL,
+--     regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
+--     populacao Decimal(5, 2) NOT NULL,
+--     Primary Key (id),
+--     UNIQUE KEY (nome),
+--     UNIQUE KEY (sigla)
+-- );
+
+
+DESCRIBE estados;
+
+
