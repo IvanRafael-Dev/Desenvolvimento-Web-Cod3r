5eca8ab295e08adbb099995f1a0acf97d4fe6c9e
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-16 11:59:35 -0300

    Inserir dados com ID / auto-increment

---
+++ 492_InserirEstadosComId/inserirComId.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO `estados` (id, nome, sigla, regiao, populacao)
+-- VALUES (1313, 'Lothlorien', 'LL', 'Centro-Oeste', 0.3)
+
+-- INSERT INTO `estados` (nome, sigla, regiao, populacao)
+-- VALUES ('Gondor', 'GD', 'Sul', 2.9)
+
+SELECT * FROM `estados`;
